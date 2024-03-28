f3966b06e472534bb19a6854ad26b549fcbe9287
Joseph Amukun <josephamukun@tutanota.com>
2024-03-28 20:55:17 +0300

    Implement `17. Sibling` (#10)

--- binary_trees.h
+++ binary_trees.h
@@ -78,4 +78,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 /* Checks if a binary tree is perfect */
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
+/* Measures the size of a binary tree */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 #endif
