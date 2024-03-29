5481f5d50d4ad1fb29fa43dacdcf730a3b070949
Ilef01 <bchinyyilef@gmail.com>
2024-03-29 08:08:09 -0700

    particular cases fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -78,12 +78,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree || !binary_tree_is_full(tree))
 		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
 	if (binary_tree_height(tree->right) == binary_tree_height(tree->left))
 	{
-		binary_tree_is_perfect(tree->left);
-		binary_tree_is_perfect(tree->right);
+		if (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right))
+			return (1);
 	}
-	else
-		return (0);
-	return (1);
+	return (0);
 }
